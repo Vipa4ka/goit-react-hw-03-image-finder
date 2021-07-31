fca274dfe2acc0c026047b95014c7898412a89d5
Your Name <you@example.com>
2021-07-31 12:28:45 +0300

    HW3.2

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+// import { v4 as uuidv4 } from "uuid";
+// import PropTypes from 'prop-types'
+
+class App extends Component {
+  state = {};
+
+  // componentDidMount() {
+  // const contacts = localStorage.getItem("contacts");
+  // const parsedContacts = JSON.parse(contacts);
+
+  // if (parsedContacts) {
+  //   this.setState({ contacts: parsedContacts });
+  // }
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.contacts !== prevState.contacts) {
+  //     localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+  //   }
+  // }
+
+  render() {
+    return;
+  }
+}
+
+export default App;
